15c7ef6837a7556e5a75c3a1bf19d8dbb7eb593b
Ishan Khatri <ishan@khatri.io>
2018-07-31 10:10:22 -0400

    Add project files.

---
+++ TFSharp-Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TFSharp_Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
